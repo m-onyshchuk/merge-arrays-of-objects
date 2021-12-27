fd44ac1dd8f1e0c24549539c4c4b91b6f4cab647
N.Onishchuk <n.a.onischyuk@diacare-soft.ru>
2021-12-27 23:21:55 +0300

    0.0.2 - new tests

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 ### Merge two array of objects
-To merge you need setup object key (identifier) to compare two objects. 
-Without key object will be compared by md5 hash.
+To merge you need setup object identifier to compare two objects. 
+Without identifier objects will be compared by md5 hash.
 
 #### Example 1
-Simple data, no identifier. 
+Simple arrays; no identifier. 
 ```
+const { arrMerge } = require('merge-arrays-of-objects');
 let original = [1, 2, 3];
 let update = [3, 4, 5];
 let merged = arrMerge(original, update);
@@ -13,11 +14,64 @@ console.log(merged);
 ```
 
 #### Example 2
-Objects array, identifier as string.
+Objects arrays; identifier as string; updating old values;
 ```
+const { arrMerge } = require('merge-arrays-of-objects');
 let original = [{id:1, value:'A'}, {id:2, value:'B'}];
-let update = [{id:2, value:'B'}, {id:3, value:'C'}];
+let update = [{id:2, value:'C'}, {id:3, value:'D'}];
 let merged = arrMerge(original, update, 'id');
 console.log(merged); 
-// [{id:1, value:'A'}, {id:2, value:'B'}, {id:3, value:'C'}]
+// [{id:1, value:'A'}, {id:2, value:'C'}, {id:3, value:'D'}]
+```
+
+#### Example 3
+Objects arrays; identifier as string; update object structure.
+```
+const { arrMerge } = require('merge-arrays-of-objects');
+let original = [{id:1, value:'A'}, {id:2, value:'B'}];
+let update = [{id:2}, {id:3, value:'D'}];
+let merged = arrMerge(original, update, 'id');
+console.log(merged); 
+// [{id:1, value:'A'}, {id:2}, {id:3, value:'D'}]
+```
+
+#### Example 4
+Objects arrays; no identifier.
+```
+const { arrMerge } = require('merge-arrays-of-objects');
+let original = [{value:'A', text:'text-A'}, {value:'B', key:42}];
+let update = [{value:'B', key:42}, {value:'D'}];
+let merged = arrMerge(original, update);
+console.log(merged); 
+// [{value:'A', text:'text-A'}, {value:'B', key:42}, {value:'D'}]
+```
+
+#### Example 5
+Objects arrays; identifier as function.
+```
+const { arrMerge } = require('merge-arrays-of-objects');
+
+let original = [
+    {key:'A', text:'text-A', child: {key: 'A'}}, 
+    {key:'A', text:'text-B', child: {key: 'B'}}
+  ];
+  
+let update = [
+    {key:'A', text:'text-C', child: {key: 'B'}}, 
+    {key:'B', text:'text-D', child: {key: 'A'}}
+  ];
+
+function key(item) {
+    return `${item.key}${item.child.key}`;
+}  
+  
+let merged = arrMerge(original, update, key);
+
+console.log(merged); 
+
+// [
+//   {key:'A', text:'text-A', child: {key: 'A'}}, 
+//   {key:'A', text:'text-C', child: {key: 'B'}}, 
+//   {key:'B', text:'text-D', child: {key: 'A'}}
+// ]
 ```
--- test.js
+++ test.js
@@ -9,34 +9,69 @@ function check(arr1, arr2) {
 
 let testSet = [
   {
-    name: 'test 1',
+    name: 'Test 1',
+    description: 'Simple arrays; no identifier.',
     arr1: [1, 2, 3],
     arr2: [3, 4, 5],
     identifier: null,
     expect: [1, 2, 3, 4, 5]
   },
   {
-    name: 'test 2',
+    name: 'Test 2',
+    description: 'Objects arrays; identifier as string; updating old values.',
     arr1: [{id:1, value:'A'}, {id:2, value:'B'}],
-    arr2: [{id:2, value:'B'}, {id:3, value:'C'}],
+    arr2: [{id:2, value:'C'}, {id:3, value:'D'}],
     identifier: 'id',
-    expect: [{id:1, value:'A'}, {id:2, value:'B'}, {id:3, value:'C'}]
+    expect: [{id:1, value:'A'}, {id:2, value:'C'}, {id:3, value:'D'}]
   },
-
+  {
+    name: 'Test 3',
+    description: 'Objects arrays; identifier as string; update object structure.',
+    arr1: [{id:1, value:'A'}, {id:2, value:'B'}],
+    arr2: [{id:2}, {id:3, value:'D'}],
+    identifier: 'id',
+    expect: [{id:1, value:'A'}, {id:2}, {id:3, value:'D'}]
+  },
+  {
+    name: 'Test 4',
+    description: 'Objects arrays; no identifier.',
+    arr1: [{value:'A', text:'text-A'}, {value:'B', key:42}],
+    arr2: [{value:'B', key:42}, {value:'D'}],
+    identifier: null,
+    expect: [{value:'A', text:'text-A'}, {value:'B', key:42}, {value:'D'}]
+  },
+  {
+    name: 'Test 5',
+    description: 'Objects arrays; identifier as function.',
+    arr1: [{key:'A', text:'text-A', child: {key: 'A'}}, {key:'A', text:'text-B', child: {key: 'B'}}],
+    arr2: [{key:'A', text:'text-C', child: {key: 'B'}}, {key:'B', text:'text-D', child: {key: 'A'}}],
+    identifier: item => `${item.key}${item.child.key}`,
+    expect: [{key:'A', text:'text-A', child: {key: 'A'}}, {key:'A', text:'text-C', child: {key: 'B'}}, {key:'B', text:'text-D', child: {key: 'A'}}]
+  },
 ];
 
-console.log('=== test start ===');
+console.log('==== test start ====');
 console.log('');
 
+let countOK = 0;
+let countFailed = 0;
 for (let test of testSet) {
-  console.log(`--- ${test.name} ---`);
+  console.log(`---- ${test.name} ----`);
+  console.log(test.description);
   console.log('arr1: ', test.arr1);
   console.log('arr2: ', test.arr2);
   console.log('expect: ', test.expect);
   let merged = arrMerge(test.arr1, test.arr2, test.identifier);
   console.log('merge:  ', merged);
-  console.log('result: ', check(test.expect, merged));
+  let result = check(test.expect, merged);
+  if (result === 'OK') {
+    countOK++;
+  } else {
+    countFailed++;
+  }
+  console.log('result: ', result);
   console.log('');
 }
 
-console.log('=== test end ===');
+console.log(`OK: ${countOK}; Failed: ${countFailed};`);
+console.log('==== test end ====');
